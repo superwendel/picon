0029140bc25e606a62a3d906d2d61c2600aecf57
Steven Wendel <superwendel@gmail.com>
2025-03-08 14:04:06 -0500

    all update

--- src/all.c
+++ src/all.c
@@ -1,4 +1,12 @@
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "defs.h"
+#include "structs.h"
+
+#include "init.c"
 #include "main.c"
